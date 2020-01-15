8b64232f94465f26d8d43fb230e26b17f0a9eb80
RVF-3019 <55239151+RVF-3019@users.noreply.github.com>
2020-01-15 10:45:59 -0500

    equals function created

--- MyArrayList.java
+++ MyArrayList.java
@@ -88,11 +88,12 @@ public class MyArrayList {
 		
 	}
 	
-	public static void main(String[] args){
-		int[] x = {1,2,3};
-		int[] y = {1,2,3};
-		System.out.println(x.equals(y));
-	}
+	public boolean equals(Object obj){
+		
+		if(obj instance Object == false){
+			return false;
+		}
+		
 	
-
+	
 }
